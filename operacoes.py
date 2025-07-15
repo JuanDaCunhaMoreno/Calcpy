292d06558a514f84fde86099620823f278765637
Juan Moreno <juandacunhamoreno@gmail.com>
2025-07-15 14:31:08 -0300

    Create operacoes.py

---
+++ operacoes.py
@@ -0,0 +1,35 @@
+#Algumas opreções
+
+def somar(*numeros):
+    total = 0
+    for n in numeros:
+        total += n
+    return total
+
+
+def subtrair(*numeros):
+    if not numeros:
+        return 0
+    total = numeros[0]
+    for n in numeros[1:]:
+        total -= n
+    return total
+
+
+def multiplicar(*numeros):
+    total = 1
+    for n in numeros:
+        total *= n
+    return total
+
+
+def dividir(*numeros):
+    if not numeros:
+        return None
+    total = numeros[0]
+    for n in numeros[1:]:
+        if n == 0:
+            raise ValueError("Divisão por zero não é permitida!")
+        total /= n
+    return total
+        
